fdabf3311a1b03626ff52f50837688d1d32c2e1a
涼雅 <mznryg@gmail.com>
2022-10-29 23:22:46 +0900

    update: NavigationControllerでラップするよう修正

--- App/LocationNote/LocationNote/Screen/Main/MainViewController.swift
+++ App/LocationNote/LocationNote/Screen/Main/MainViewController.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class MainViewController: BaseViewController {
 
-    static func initFromStoryboard() -> MainViewController {
+    static func initFromStoryboard() -> UIViewController {
         let storyboard = UIStoryboard(name: R.storyboard.main.name, bundle: nil)
         let viewController = storyboard.instantiateInitialViewController() as! MainViewController
 
-        return viewController
+        return UINavigationController(rootViewController: viewController)
     }
 
     override func viewDidLoad() {
