a594e1334d8164162c37bf4e5588691b7e04320d
涼雅 <mznryg@gmail.com>
2022-11-05 23:31:53 +0900

    update: NavigationBarに閉じるボタンを追加

--- App/LocationNote/LocationNote/Screen/Memo/AddMemoViewController.swift
+++ App/LocationNote/LocationNote/Screen/Memo/AddMemoViewController.swift
@@ -15,6 +15,8 @@ class AddMemoViewController: BaseViewController {
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var addButton: PrimaryButton!
 
+    private var closeButtonItem: UIBarButtonItem!
+
     static func initFromStoryboard() -> UIViewController {
         let storyboard = UIStoryboard(name: R.storyboard.addMemo.name, bundle: nil)
         let viewController = storyboard.instantiateInitialViewController() as! AddMemoViewController
@@ -26,6 +28,16 @@ class AddMemoViewController: BaseViewController {
         super.viewDidLoad()
         title = "メモ追加"
 
+        closeButtonItem = UIBarButtonItem(barButtonSystemItem: .stop, target: self, action: #selector(closeButtonTapped(_:)))
+        closeButtonItem.tintColor = R.color.white1()
+        self.navigationItem.rightBarButtonItem = closeButtonItem
+    }
+
+}
+
+extension AddMemoViewController {
+    @objc func closeButtonTapped(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true)
     }
 
 }
