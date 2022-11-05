1850834c9a5020bdef3af0476d61a2731edce132
涼雅 <mznryg@gmail.com>
2022-11-05 22:44:56 +0900

    update: ViewControllerの初期化処理を実装

--- App/LocationNote/LocationNote/Screen/Memo/AddMemoViewController.swift
+++ App/LocationNote/LocationNote/Screen/Memo/AddMemoViewController.swift
@@ -15,8 +15,16 @@ class AddMemoViewController: BaseViewController {
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var addButton: PrimaryButton!
 
+    static func initFromStoryboard() -> UIViewController {
+        let storyboard = UIStoryboard(name: R.storyboard.addMemo.name, bundle: nil)
+        let viewController = storyboard.instantiateInitialViewController() as! AddMemoViewController
+
+        return NavigationViewController.init(rootViewController: viewController)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "メモ追加"
 
     }
 
