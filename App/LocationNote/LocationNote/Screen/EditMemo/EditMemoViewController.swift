a81871d378da73343d98bc38d1db0fb85b7628f6
涼雅 <mznryg@gmail.com>
2022-11-20 21:50:11 +0900

    update: viewModel経由で削除処理を行うよう実装

--- App/LocationNote/LocationNote/Screen/EditMemo/EditMemoViewController.swift
+++ App/LocationNote/LocationNote/Screen/EditMemo/EditMemoViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import CoreLocation
+import RxSwift
+import RxCocoa
 
 // MARK: LifeCycle
 class EditMemoViewController: BaseViewController {
@@ -14,11 +16,12 @@ class EditMemoViewController: BaseViewController {
     @IBOutlet weak var detailTextView: UITextView!
     @IBOutlet weak var tagTextField: UITextField!
     @IBOutlet weak var locationLabel: UILabel!
-    @IBOutlet weak var addButton: PrimaryButton!
+    @IBOutlet weak var editButton: PrimaryButton!
 
     private var memo: Memo?
 
-    private let viewModel = EditMemoViewModel()
+    private var viewModel: EditMemoViewModel?
+    private let disposeBag = DisposeBag()
 
     static func initFromStoryboard(memo: Memo, parent: UIAdaptivePresentationControllerDelegate) -> UIViewController {
         let storyboard = UIStoryboard(name: R.storyboard.editMemoViewController.name, bundle: nil)
@@ -32,10 +35,12 @@ class EditMemoViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewModel = EditMemoViewModel(memo: self.memo!)
         title = "編集"
 
         setNavigationBarItem()
         setLayout()
+        bind()
     }
 }
 
@@ -73,27 +78,60 @@ extension EditMemoViewController {
 }
 
 // MARK: Navigation View Button
-extension EditMemoViewController{
-    
+extension EditMemoViewController {
+
     @objc func closeButtonTapped(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true)
     }
 
     @objc func deleteButtonTapped(_ sender: UIBarButtonItem) {
-        self.showAleart(title: "このメモを削除しますか？", subtitle: nil, confirmButtonTitle: "削除", onConfirm: { self.dismissWithDelete() })
+        self.showAleart(title: "このメモを削除しますか？", subtitle: nil, confirmButtonTitle: "削除", onConfirm: {
+            self.viewModel?.onDeleteButtonTapped()
+            self.dismiss(animated: true)
+        })
     }
-    
-    func dismissWithDelete() {
-        let deleteMemo = Memo(title: titleTextField.text ?? "", detail: detailTextView.text ?? "", tag: tagTextField.text ?? "", latitude: self.memo!.latitude, longitude: self.memo!.longitude)
-        viewModel.onDeleteButtonTapped(memo: deleteMemo)
-        self.dismiss(animated: true)
-    }
-    
+
     override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
         guard let presentationController = presentationController else {
             return
         }
         presentationController.delegate?.presentationControllerDidDismiss?(presentationController)
         super.dismiss(animated: flag, completion: completion)
+    }
+}
+
+extension EditMemoViewController {
+    func bind() {
+        guard let viewModel = self.viewModel else {
+            return
+        }
+
+        titleTextField.rx.text.orEmpty
+            .asDriver()
+            .drive(viewModel.title)
+            .disposed(by: disposeBag)
+
+        detailTextView.rx.text.orEmpty
+            .asDriver()
+            .drive(viewModel.detail)
+            .disposed(by: disposeBag)
+
+        tagTextField.rx.text.orEmpty
+            .asDriver()
+            .drive(viewModel.tag)
+            .disposed(by: disposeBag)
+
+        editButton.rx.tap
+            .asDriver()
+            .drive(onNext: {
+//                self.viewModel?.onAddButtonTapped()
+                self.dismiss(animated: true)
+            })
+            .disposed(by: disposeBag)
+
+        viewModel.buttonEnabled
+            .drive(editButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+
     }
 }
